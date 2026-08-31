3df81c43863bc573db1f8422a3128adc3edafd44
Nazarul <nazarulxfitri@gmail.com>
2026-09-01 00:31:25 +0800

    fix: ensure PrismaClient fallback checks for new Event model

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -3,9 +3,10 @@ import { PrismaClient } from '@prisma/client';
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
 export const prisma =
-  globalForPrisma.prisma ||
-  new PrismaClient({
-    log: ['error'],
-  });
+  globalForPrisma.prisma && (globalForPrisma.prisma as any).event
+    ? globalForPrisma.prisma
+    : new PrismaClient({
+        log: ['error'],
+      });
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
